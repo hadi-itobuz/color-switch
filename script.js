2b184aac609ffead0b054965de501210a35efa59
hadi <hadi@itobuz.com>
2024-12-26 18:18:17 +0530

    FEAT: color switch

---
+++ script.js
@@ -0,0 +1,15 @@
+const setColour=(color)=>{
+    document.body.style.backgroundColor=color;    
+}
+const aqua=()=>{
+    setColour("aqua");
+}
+const green=()=>{
+    setColour("green");
+}
+const gray=()=>{
+    setColour("gray");
+}
+const blue=()=>{
+    setColour("blue");
+}
